bc1c02ebf29ebbe04963f893cc88d230b3e09ac3
Cole Willsea <coleww@gmail.com>
2015-09-25 07:43:48 -0700

    umm dont let it stop or end with a stopword

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ var markov = require('@coleww/markov')
 var Twit = require('twit')
 var addEnder = require('add-ender')
 var capitalize = require('capitalize')
-
+var stopwords = require('stopwords').english
 module.exports = function (lines, config, n, cb) {
   var T = new Twit(config)
   var m = markov(n || 2)
 
   var init = after(lines.length, function () {
-    var toot = !!cb ? cb(m) : addEnder(capitalize(m.fill(m.pick(), 12).join(" ").toLowerCase()))
+
+
+
+    var start = m.pick()
+    while (stopwords.indexOf(start.toLowerCase()) !== -1) start = m.pick()
+
+    var toot = !!cb ? cb(m) : addEnder(capitalize(m.fill(, 12).join(" ").toLowerCase()))
+
+    while (stopwords.indexOf(toot.split(' ')[toot.split(' ').length - 1].toLowerCase()) !== -1) toot = toot.split(' ').slice(0, toot.split(' ').length - 1).join(' ')
     console.log(toot)
-    if(iscool(toot) && toot.length < 140){
+    if(iscool(toot) && toot.length < 140 && toot.length > 0){
       T.post('statuses/update', {status: toot}, function (err, data, response) {
         console.log(err)
         console.log(data)
